c550437d75a40fcb1e573a692417871f938f32b5
yuyusss <97606045+yuyusss@users.noreply.github.com>
2022-04-03 00:08:23 +0900

    Update TodoTemplate.js

--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -4,7 +4,7 @@ import './TodoTemplate.scss';
 const TodoTemplate = ({ children }) => {
     return (
         <div className='TodoTemplate'>
-            <div className="app-title">일정 관리</div>
+            <div className="app-title">일정 관리</div> 
             <div className="content">{children}</div>
         </div>
     );
